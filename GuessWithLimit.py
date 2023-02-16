5773efe4a2200092e1111421f71fbf4dbc4927d0
billybboy <kk00915@live.com>
2023-02-16 14:14:33 +0800

    猜數字限制次數版本

---
+++ GuessWithLimit.py
@@ -0,0 +1,20 @@
+"""從1-100猜數字"""
+import random
+num = int(input("please enter a number in 1-100: "))
+count = 0 #設定初始猜測次數
+count_limit = 6
+target = random.randint(1, 101) #建立1-100隨機數
+while num != target and count <= count_limit:
+    if num > target:
+        print("too high")
+        num = int(input("please enter another number in 1-100: "))
+        count += 1
+    if num < target:
+        print("too low")
+        num = int(input("please enter another number in 1-100: "))
+        count += 1
+    if num == target:
+        print("bingo!")
+        print("guess count: " + str(count))
+if count > count_limit:
+    print("sorry you lose")
